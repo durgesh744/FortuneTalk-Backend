ede37694f79e50c7a8ab3d023ea698b8529dcd61
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-04-14 14:14:53 +0530

    login flow done

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -19,6 +19,13 @@ const LoginWithGoogle = asyncHandler(async (req, res) => {
     res.status(201).json({ success: true, data: user });
 })
 
+/**
+ * This function is used to createAccount a user
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @returns {Object} Returns created user object
+ */
+
 const LoginWithFacebook = asyncHandler(async (req, res) => {
     let user = await Auth.findOne({ fb_id: req.body.fb_id })
     if (user) {
@@ -27,6 +34,13 @@ const LoginWithFacebook = asyncHandler(async (req, res) => {
     }
     res.status(201).json({ success: true, data: user });
 });
+
+/**
+ * This function is used to createAccount a user and send otp
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @returns {Object} Returns created user object
+ */
 
 const send_OTP = asyncHandler(async (req, res) => {
     let user = await Auth.findOne({ phone: req.query.number })
@@ -38,10 +52,24 @@ const send_OTP = asyncHandler(async (req, res) => {
     res.status(200).send({ success: true, otp, user });
 })
 
+/**
+ * This function is used to get user by query
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @returns {Object} Returns created user object
+ */
+
 const getUser = asyncHandler(async (req, res) => {
     console.log(req.query.id, "lkndskjnf")
     res.status(200).send({ success: true, user: await Auth.findOne({ _id: req.query.id }) })
 })
+
+/**
+ * This function is used to update user
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @returns {Object} Returns created user object
+ */
 
 const UpdateUser = asyncHandler(async (req, res) => {
     console.log(req.params.id)
